a1bba276a77ebd6b9dce08e811f91920e61e5d23
Gitago <76603653+gitagogaming@users.noreply.github.com>
2023-07-30 20:04:40 -0400

    version update

--- TPPEntry.py
+++ TPPEntry.py
@@ -7,7 +7,7 @@ PLUGIN_ICON ="websockets_logo.png"
 
 TP_PLUGIN_INFO = {
     "sdk": 6,
-    "version": 103,
+    "version": 104,
     "name": "Websockets",
     "id": PLUGIN_ID,
     "plugin_start_cmd_windows": "%TP_PLUGIN_FOLDER%Websocket Plugin\\Websockets_TP.exe",
